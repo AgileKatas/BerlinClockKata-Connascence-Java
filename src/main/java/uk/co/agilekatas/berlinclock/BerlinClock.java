fdc407ce9a7314e69369dcf61191d9fc59c53776
Agile Katas <contact@agilekatas.co.uk>
2016-12-08 15:11:52 +0000

    Naive pass of the test

--- src/main/java/uk/co/agilekatas/berlinclock/BerlinClock.java
+++ src/main/java/uk/co/agilekatas/berlinclock/BerlinClock.java
@@ -15,8 +15,8 @@ public class BerlinClock {
 
   public String getTime() {
     if (calendar.get(Calendar.SECOND) % 2 == 0) {
-      return YELLOW_LAMP;
+      return YELLOW_LAMP + OFF_LAMP + OFF_LAMP + OFF_LAMP + OFF_LAMP;
     }
-    return OFF_LAMP;
+    return OFF_LAMP + OFF_LAMP + OFF_LAMP + OFF_LAMP + OFF_LAMP;
   }
 }
